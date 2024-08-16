fd4f4d880a25974a6e9142857b6a4c44a8e32d32
jh5H <ojh2079@rlmodel.com>
2024-08-16 14:50:58 +0900

    updated rules

--- witmotion_ros/scripts/create_udev_rules.sh
+++ witmotion_ros/scripts/create_udev_rules.sh
@@ -4,13 +4,13 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 echo -e "${GREEN}==== Create udev rules for SRC ====${NC}"
-if [ -d /etc/udev/rules.d/src_udev.rules ]
+if [ -d /etc/udev/rules.d/imu_udev.rules ]
 then
 	echo -e "${RED} delete exist file ${NC}"
 	#rm -rf /etc/udev/rules.d/src_udev.rules
 fi
 
-sudo cp src_udev.rules /etc/udev/rules.d/src_udev.rules
+sudo cp imu_udev.rules /etc/udev/rules.d/imu_udev.rules
 echo " "
 echo "reload udev rules"
 sudo udevadm control --reload-rules && sudo udevadm trigger
